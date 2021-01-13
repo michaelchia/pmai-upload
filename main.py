818ee947e86de456c228454a4909cc35f8280eee
michael <michael_chia7@msn.com>
2021-01-13 19:20:41 +0800

    upload files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import itertools
 import requests
 import json
+import time
+import io
 
 import pdblp
 
@@ -20,7 +22,7 @@ TYPE_MAP = {
 }
 XCCY = {
     "type": "BS",
-    "ccys": ["JY", "EU", "AD", "ND", "GB", "HD", "SD"],
+    "ccys": ["JY", "EU", "AD", "ND", "BP", "HD", "SD"],
     "periods": [1, 2, 3, 4, 5, 7, 10]
 }
 IRS = {
@@ -39,17 +41,18 @@ START_DATE = "20140101"
 BUCKET_NAME = "blpdata"
 FOLDER = "raw/"
 
-CRED_PATH = "cred.json"
+CRED_PATH = "cred.json.txt"
 
 UPLOAD_URL = "https://pmai-297814.et.r.appspot.com/upload"
+
 
 def main():
     # password
     with open(CRED_PATH) as f:
-        auth = json.load(f)
+        auth = tuple(json.load(f))
 
     # blp
-    blp = pdblp.BCon(timeout=5000)
+    blp = pdblp.BCon(timeout=10000)
     blp.start()
 
     tickers = []
@@ -61,21 +64,35 @@ def main():
             in itertools.product(ccys, periods)
         ]
 
-    df = blp.bdh(tickers, "PX_LAST", start_date=START_DATE, end_date="20990101")
-    df.columns = list(df.columns.get_level_values(0))
+    def _get_data(ticker):
+        try:
+            df = blp.bdh(ticker, "PX_LAST", start_date=START_DATE, end_date="20990101")
+        except (ValueError, RuntimeError):
+            print("retry " + ticker)
+            time.sleep(2)
+            df = blp.bdh(ticker, "PX_LAST", start_date=START_DATE, end_date="20990101")
+        df.columns = list(df.columns.get_level_values(0))
+        return df
 
-    for ticker in tickers:
-        _upload_column(df, ticker)
+    dfs = {ticker: _get_data(ticker)
+           for ticker in tickers}
+
+    for ticker, df in dfs.items():
+        _upload_df(df, ticker, auth)
+
+def _upload_df(df, ticker, auth):
+    file = io.StringIO()
+    file.filename = ticker + ".csv"
+    df.to_csv(file, line_terminator="\n")
+    file.seek(0)
+    _upload_file(file, ticker, auth)
 
 
-def _upload_column(df, ticker):
-    df = df[[ticker]]
-
-
-def _upload_file(file, auth):
+def _upload_file(file, ticker, auth):
     resp = requests.post(
-        "http://127.0.0.1:8080/upload",
-        files={"file": file},
-        auth=auth)
+        UPLOAD_URL,
+        files={ticker: file},
+        auth=auth,
+        verify=False)
     if resp.status_code != 200:
         resp.raise_for_status()
